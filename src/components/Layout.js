33b6df4c30c7ffc9f52b5ae831e18089eb13fb18
Yesenia71 <yese3982@gmail.com>
2021-09-16 23:23:41 -0500

    agregar los personajes y su descripcion

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,11 +4,14 @@ const Layout = () => {
     return (
         <>
             {personajes.map((personaje, index) => (
-                <div className="card" key={index} style={{ width: '18rem' }}>
+                <div className="card-img-top" key={index} style={{ width: '200rem' }}>
                     <img src={personaje.imagen} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{personaje.nombre}</h5>
                         <p className="card-text">{personaje.descripcion}</p>
+                    </div>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
                     </div>
                 </div>
             ))}
--- src/components/Personajes.js
+++ src/components/Personajes.js
@@ -1,23 +1,24 @@
 export const personajes = [
     {
-        nombre: 'Luz Noceda',
-        descripcion: 'Es la protagonista principal de The Owl House. Es una chica adolescente de origen dominicano-estadounidense que accidentalmente tropezó a un portal a las Islas Hirvientes, una dimensión donde la magia es real, y los humanos son tratados como seres inferiores',
+        nombre: 'L Lawliet',
+        descripcion: 'L cuyo verdadero nombre es L Lawliet es el misterioso detective que toma la misión de atrapar a Kira. Aunque en un principio se desconoce su rostro y nombre, a lo largo de la serie acaba presentándose ante otros personajes, desvelando así su extraña personalidad y convirtiéndose en uno de los personajes principales de la serie.',
         imagen: 'https://media.nyusu.fm/ranking/option/940/32491.jpg'
     },
     {
-        nombre: 'Eda Clawthorne',
-        descripcion: 'Eda Clawthorne es la deuteragonista de The Owl House. Ella es una bruja rebelde con una recompensa de un billón de caracoles por su captura.',
+        nombre: 'Light Yagami',
+        descripcion: 'Light Yagami  también conocido como Kira, es el protagonista principal de la serie. Light encuentra la Death Note y la utiliza para matar a los delincuentes que principalmente aparecían en las noticias, luego comenzó a matar criminales. Después pasó a ser conocido en Japón como Kira. Se unió a la policía para acercarse a su enemigo L y acabar con él.',
         imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVYupSNS3D40IiEWWivVWl6bxcU9oj-Zcd8g&usqp=CAU'
     },
     {
-        nombre: 'King',
-        descripcion: 'King es el tritagonista de The Owl House. Es un pequeño demonio que afirma haber sido un rey, como sugiere su nombre, pero ahora es el compañero de cuarto de Eda y busca restaurar su título y gloria.',
+        nombre: 'Misa Amane',
+        descripcion: 'Misa Amane es uno de los personajes principales de Death Note, quien además de ser una reconocida Idol, es el "Segundo Kira" y la novia de Light Yagami.Misa suele tener una actitud algo infantil, ingenua e impulsiva. También tiene un fuerte temperamento, como se ve en los momentos de celos por Light y los comentarios de L hacia ella. Cuando ella habla suele referirse así misma en tercera persona. Siente una determinante devoción y admiración hacia Kira por matar al asesino de sus padres',
         imagen: 'http://pm1.narvii.com/6409/96db34dddf2dfe3009ae90351aa3f27a87059320_00.jpg'
     },
     {
-        nombre: 'Amity Blight',
-        descripcion: 'Amity Blight es una joven bruja prodigiosa que asiste a la Escuela de Magia y Demonios Hexside.',
+        nombre: 'Ryuk',
+        descripcion: 's un shinigami que entrega indirectamente una Death Note a Light Yagami. Al estar aburrido en el Mundo Shinigami, consigue robar una segunda Death Note y la deja caer al Mundo Humano para que alguien la encuentre, todo esto con el objetivo de encontrar entretenimiento, cosa inexistente para él en su mundo. Esa Death Note fue recogida por Light, que la utiliza para intentar crear un mundo nuevo, libre de criminales y personas malvadas.',
         imagen: 'https://pbs.twimg.com/profile_images/3311548752/e4b8df091bd291524f05e83218df666f_400x400.jpeg'
-    }
+    },
+  
 ]
 
